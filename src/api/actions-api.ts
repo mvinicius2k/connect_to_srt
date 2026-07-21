648c5a8cfec7a20336d410832ec30411bd4a7ce0
mvnicius2k <marcos.vinicius124816@gmail.com>
2026-07-21 12:32:24 -0300

    Simplifica mockagem

--- src/api/actions-api.ts
+++ src/api/actions-api.ts
@@ -1,18 +1,44 @@
-import { defineCommands, ErrorIdentifier, TauriCommand } from "@/api/tauri";
 import { SRTPoint } from "@/components/srt-points-viewer/shared";
-import { ActionsMock } from "@/mock/actions-mock";
+import { Result, ResultAction } from "@/helpers/result";
+import { sleep } from "@/helpers/utils";
 
-export type ScanSucessResponseDto = SRTPoint[];
+import { invoke } from "@tauri-apps/api/core";
+import { times } from "remeda";
+
+
+export type ErrorIdentifier<TDetails = never> =
+  [TDetails] extends [never]
+    ? { code: string }
+    : { code: string; details: TDetails };
+export type ScanSucessResponseDto = { points: SRTPoint[] };
 export type ScanFailResponseDto = ErrorIdentifier;
 
-export type PlayRequestDto = { point: SRTPoint }
-export type PlaySucessResponseDto = undefined;
+export type PlayRequestDto = { url: string };
 export type PlayFailResponseDto = ErrorIdentifier;
-export const ActionsAPI = defineCommands({
-  scan_command: {
-    mock: ActionsMock.scan,
-  } satisfies TauriCommand<undefined, ScanSucessResponseDto, ScanFailResponseDto>,
-  play_command: {
-    mock: ActionsMock.play,
-  }  satisfies TauriCommand<PlayRequestDto, PlaySucessResponseDto, PlayFailResponseDto>,
-});
+
+export const ActionsAPI = {
+  async scan(): Promise<
+    ResultAction<ScanSucessResponseDto, ScanFailResponseDto>
+  > {
+    if (import.meta.env.VITE_MOCK) {
+      const { faker } = await import("@faker-js/faker");
+      await sleep(1000)
+      const points = times(
+        faker.number.int({ min: 0, max: 15 }),
+        (): SRTPoint => ({
+          host: faker.internet.ipv4(),
+          port: faker.number.int({ min: 1000, max: 60_000 }),
+          kind: faker.helpers.arrayElement(["listener", "rendezvous"]),
+        }),
+      );
+      return Result.success({points});
+    }
+    return invoke<ResultAction<ScanSucessResponseDto,ScanFailResponseDto>>("scan_command");
+  },
+  async play(dto: PlayRequestDto): Promise<ResultAction<undefined, PlayFailResponseDto>> {
+    if (import.meta.env.VITE_MOCK)
+      return Result.success(undefined)
+    return invoke<ResultAction<undefined, PlayFailResponseDto>>('play_command', dto)
+
+  }
+};
